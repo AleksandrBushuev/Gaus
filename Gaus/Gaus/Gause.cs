d02fffbfc124af072817cb10c1d099bfb35831c5
Aleksandr Bushuev <Aleksandr.Bushuev.97@mail.ru>
2022-05-23 11:56:48 +0500

    Реализация метода Гаусса

--- Gaus/Gaus/Gause.cs
+++ Gaus/Gaus/Gause.cs
@@ -1,16 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Gaus
 {
-    public class Gause
-    {
-        private double[,] _matrix;
-
-        public Gause(double[,] matrix)
-        {
-            _matrix = matrix;
-        }
-
+    public static class Gause
+    {        
         /// <summary>
         /// Создать матрицу
         /// </summary>
@@ -38,6 +32,55 @@ namespace Gaus
             return matrix;
         }
 
+        public static double[] Calculate(double[,] matrix)
+        {
+            int countRows = matrix.GetLength(0); 
+            double[,] buffer = new double[countRows, countRows + 1]; 
+
+            for (int i = 0; i < countRows; i++)
+                for (int j = 0; j < countRows + 1; j++)
+                    buffer[i, j] = matrix[i, j];
+
+            //Прямой ход   
+            Parallel.For(0, countRows, numberRow =>
+            {
+                for (int numberColumn = 0; numberColumn < countRows + 1; numberColumn++)
+                    buffer[numberRow, numberColumn] = buffer[numberRow, numberColumn] / matrix[numberRow, numberRow];
+
+                for (int numberRowNext = numberRow + 1; numberRowNext < countRows; numberRowNext++)
+                {
+                    double m = buffer[numberRowNext, numberRow] / buffer[numberRow, numberRow];
+
+                    for (int numberColumnNext = 0; numberColumnNext < countRows + 1; numberColumnNext++)
+                        buffer[numberRowNext, numberColumnNext] = buffer[numberRowNext, numberColumnNext] - buffer[numberRow, numberColumnNext] * m; //Зануление элементов матрицы ниже первого члена, преобразованного в единицу
+                }
+
+
+                for (int i = 0; i < countRows; i++)
+                    for (int j = 0; j < countRows + 1; j++)
+                        matrix[i, j] = buffer[i, j];
+            });           
+
+            //Обратный ход (Зануление верхнего правого угла)
+            for (int k = countRows - 1; k > -1; k--) //k-номер строки
+            {
+                for (int i = countRows; i > -1; i--) //i-номер столбца
+                    buffer[k, i] = buffer[k, i] / matrix[k, k];
+                for (int i = k - 1; i > -1; i--) //i-номер следующей строки после k
+                {
+                    double K = buffer[i, k] / buffer[k, k];
+                    for (int j = countRows; j > -1; j--) //j-номер столбца следующей строки после k
+                        buffer[i, j] = buffer[i, j] - buffer[k, j] * K;
+                }
+            }
+
+            
+            double[] result = new double[countRows];
+            for (int i = 0; i < countRows; i++)
+                result[i] = buffer[i, countRows];
+
+            return result;
+        }
 
     }
 }
--- Gaus/GausExample/Program.cs
+++ Gaus/GausExample/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Gaus;
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace GausExample
 {
@@ -8,13 +10,74 @@ namespace GausExample
     {
         static void Main(string[] args)
         {
-           double [,] matrix  =  Gause.CreateMatrix(3, 3, new GauseRandom(new Random()));
 
-            Gause test = new Gause(matrix);
+            //while (Console.ReadLine()=="1") {
 
+                Stopwatch st = new Stopwatch();
+
+                st.Start();
+                double[,] matrix = new double[,]
+                {
+                {3, 2, -5, -1 },
+                {2, -1, 3, 13},
+                {1, 2, -1, 9}
+                };
+
+                for (int i = 0; i < 10; i++)
+                {
+                    double[] r = Gause.Calculate(matrix);
+                }
+
+                st.Stop();
+
+
+                long t = st.ElapsedMilliseconds;
+                Console.WriteLine(t);
+           // }
             
+        }
 
 
+
+        public static void F()
+        {
+            double s = 0;
+            Console.WriteLine("Введите размерность системы");
+            int n = int.Parse(Console.ReadLine());
+            double[,] a = new double[n, n];
+            double[] b = new double[n];
+            double[] x = new double[n];
+            for (int i = 0; i < n; i++)
+                x[i] = 0;
+            Console.WriteLine("Введите построчно коэффициенты системы");
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = double.Parse(Console.ReadLine());
+                }
+            Console.WriteLine("Введите свободные коэффициенты");
+            for (int i = 0; i < n; i++)
+                b[i] = double.Parse(Console.ReadLine());
+
+            for (int k = 0; k < n - 1; k++)
+            {
+                for (int i = k + 1; i < n; i++)
+                {
+                    for (int j = k + 1; j < n; j++)
+                    {
+                        a[i, j] = a[i, j] - a[k, j] * (a[i, k] / a[k, k]);
+                    }
+                    b[i] = b[i] - b[k] * a[i, k] / a[k, k];
+                }
+            }
+            for (int k = n - 1; k >= 0; k--)
+            {
+                s = 0;
+                for (int j = k + 1; j < n; j++)
+                    s = s + a[k, j] * x[j];
+                x[k] = (b[k] - s) / a[k, k];
+            }
+           
         }
     }
 }
